5b8d0e1c7ed433b19b126e033a0ad962019158d4
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-02-13 19:41:16 +0700

    Update settings.py

--- scripts/settings.py
+++ scripts/settings.py
@@ -6,7 +6,7 @@ def on_ui_settings():
     shared.opts.add_option("civitai_link_key", shared.OptionInfo("", "Your Civitai Link Key", section=section, onchange=on_civitai_link_key_changed))
     shared.opts.add_option("civitai_link_logging", shared.OptionInfo(True, "Show Civitai Link events in the console", section=section))
     shared.opts.add_option("civitai_api_key", shared.OptionInfo("", "Your Civitai API Key", section=section))
-    shared.opts.add_option("civitai_download_previews", shared.OptionInfo(True, "Download missing preview images on startup", section=section))
+    shared.opts.add_option("civitai_download_previews", shared.OptionInfo(False, "Download missing preview images on startup", section=section))
     shared.opts.add_option("civitai_download_triggers", shared.OptionInfo(True, "Download missing activation triggers on startup", section=section))
     shared.opts.add_option("civitai_nsfw_previews", shared.OptionInfo(False, "Download NSFW (adult) preview images", section=section))
     shared.opts.add_option("civitai_download_missing_models", shared.OptionInfo(True, "Download missing models upon reading generation parameters from prompt", section=section))
